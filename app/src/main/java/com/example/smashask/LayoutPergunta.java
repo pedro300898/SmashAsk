9073bf12844062901113ceecfd7dbfb753f234bf
pedro <pedro300898@gmail.com>
2019-04-30 12:09:53 -0300

    Add: Inicial Activitys

--- app/src/main/java/com/example/smashask/LayoutPergunta.java
+++ app/src/main/java/com/example/smashask/LayoutPergunta.java
@@ -2,15 +2,12 @@ package com.example.smashask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 
-
-public class MainActivity extends AppCompatActivity {
+public class LayoutPergunta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_layout_pergunta);
     }
 }
